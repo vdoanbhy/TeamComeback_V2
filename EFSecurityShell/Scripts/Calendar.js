ed8b7d502cbfdad3b5a81bdb723294340293f950
Vu T Doan <vdoan@masonlive.gmu.edu>
2020-10-22 12:40:57 -0400

    Add jquery ui calendar, fix page titles

---
+++ EFSecurityShell/Scripts/Calendar.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    $(".dobpicker").datepicker({
+        dateFormat: "mm/dd/yy",
+        changeMonth: true,
+        changeYear: true,
+        showOn: "both",
+        buttonText: "<i class='fa fa-calendar'></i>"
+    });
+});
+
+$(function () {
+    $(".dolspicker").datepicker({
+        dateFormat: "mm/dd/yy",
+        changeMonth: true,
+        changeYear: true,
+        showOn: "both",
+        buttonText: "<i class='fa fa-calendar'></i>"
+    });
+});
