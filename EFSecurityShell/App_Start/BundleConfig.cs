5f7f4e350acbe2945e152874c70d3472c62b2f0f
Vu T Doan <vdoan@masonlive.gmu.edu>
2020-11-02 23:56:43 -0500

    no more scroll

--- EFSecurityShell/App_Start/BundleConfig.cs
+++ EFSecurityShell/App_Start/BundleConfig.cs
@@ -17,11 +17,10 @@ namespace TeamComeback_V2
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                      "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/scroll.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
