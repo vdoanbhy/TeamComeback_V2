54c587580f9840229efe7a266078f646e59558bc
Vu T Doan <vdoan@masonlive.gmu.edu>
2020-11-02 23:40:47 -0500

    Add scroll action

--- EFSecurityShell/App_Start/BundleConfig.cs
+++ EFSecurityShell/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace TeamComeback_V2
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/scroll.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ EFSecurityShell/Scripts/scroll.js
@@ -0,0 +1,12 @@
+﻿var toTop = document.querySelector(".to-top"); //access the class="to-top"
+
+
+//add function scroll() to scroll event
+window.addEventListener("scroll", function scroll() {
+    //decision structure to add and remove class="active" when scroll pass 100px
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
